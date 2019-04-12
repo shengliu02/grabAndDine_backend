7d731541604893b38a1d9757704262b812b18e87
Sheng Liu <sheng1302@gmail.com>
2019-04-12 18:12:47 -0400

    update food restrictions -> dietary options

--- models/users.js
+++ models/users.js
@@ -1,10 +1,11 @@
-const bcrypt = require("bcrypt-nodejs");
+const bcrypt = require('bcrypt-nodejs');
+
 module.exports = (sequelize, DataTypes) => {
-  const Users = sequelize.define("Users", {
+  const Users = sequelize.define('Users', {
     user_id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV1,
-      primaryKey: true
+      primaryKey: true,
     },
     email: {
       type: DataTypes.STRING,
@@ -12,66 +13,63 @@ module.exports = (sequelize, DataTypes) => {
       unique: true,
       validate: {
         notEmpty: true,
-        isEmail: true // sequelize validation
-      }
+        isEmail: true, // sequelize validation
+      },
     },
     username: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: {
-        notEmpty: true
-      }
+        notEmpty: true,
+      },
     },
     password_hash: {
       type: DataTypes.STRING,
-        allowNull: false,
+      allowNull: false,
     },
-    food_restrictions: {
+    dietary_options: {
       type: DataTypes.ARRAY(DataTypes.STRING),
       allowNull: false,
       validate: {
-        notEmpty: true
-      }
-    },
-      bios: {
-        type: DataTypes.STRING,
-          defaultValue: "Do not have a bios yet!",
-          allowNull: false,
+        notEmpty: true,
       },
-      gender: {
-          type: DataTypes.STRING,
-          defaultValue: "Unspecified",
-          allowNull: false,
-      }
-      ,
+    },
+    bios: {
+      type: DataTypes.STRING,
+      defaultValue: 'Do not have a bios yet!',
+      allowNull: false,
+    },
+    gender: {
+      type: DataTypes.STRING,
+      defaultValue: 'Unspecified',
+      allowNull: false,
+    },      
       age: {
-          type: DataTypes.INTEGER,
-          defaultValue: 0,
-          allowNull: false,
-      }
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      allowNull: false,
+    },
   });
 
   // this is a Sequelize lifecycle hook
-  Users.beforeCreate(user =>
-    new sequelize.Promise(resolve => {
+  Users.beforeCreate(user => new sequelize.Promise((resolve) => {
       bcrypt.hash(user.password_hash, null, null, (err, hashedPassword) => {
         resolve(hashedPassword);
       });
-    }).then(hashedPw => {
+    }).then((hashedPw) => {
       user.password_hash = hashedPw;
-    })
-  );
+    }),);
 
-  Users.associate = function(models) {
+  Users.associate = function (models) {
     Users.hasMany(models.Requests, {
-      foreignKey: "request_user_id",
-      as: "request_user_id"
+      foreignKey: 'request_user_id',
+      as: 'request_user_id',
     });
 
     Users.hasMany(models.Requests, {
-      foreignKey: "matched_user_id",
-      as: "matched_user_id"
+      foreignKey: 'matched_user_id',
+      as: 'matched_user_id',
     });
   };
 
