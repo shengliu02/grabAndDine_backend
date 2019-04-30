4bfe70c2afab58f18826aaacd480cdd44fd0187e
kev-th <kevinkthaw@gmail.com>
2019-04-29 23:01:30 -0400

    improve matching algo

--- controllers/requests.js
+++ controllers/requests.js
@@ -14,84 +14,74 @@ const AUTH_CONTROLLER = {
   },
 
   unauthorized(req, res) {
-    // res.status(401).json({ message: req.flash('You are not authorized to access beyond this point.') });
+    // res.status(401).json({
+    //   message: req.flash('You are not authorized to access beyond this point.')
+    // });
   },
 
   create(req, res) {
-    const BreakException = {};
     Requests.create({
       is_matched: false,
       request_user_id: req.body.request_user_id,
       location: req.body.location,
-
     })
       .then((item) => {
-        // res.status(201).json({
-        //   message: 'We are trying to match you with a mate, please wait for a second...',
-        //   itemObject: item,
-        // });
-
-        // res.status(201).json({
-        //   message: 'second',
-        // });
-        console.log('___________________________');
-        console.log(item);
-        console.log('___________________________');
         const curr = item.dataValues;
-        console.log('___________________________');
-        console.log(curr);
-
         let userHere;
         let matchedUser = 0;
+        let isMatched = false;
         Requests.findAll().then((users) => {
           if (Object.keys(users).length === 1) {
             res.status(500).json({ message: 'you are the only one' });
           }
-          console.log('!!!!!!!!!!!!!!!');
-          // console.log(users);
           users.forEach((each) => {
-            console.log('reading each');
-            if (each.dataValues.request_user_id !== curr.request_user_id && each.dataValues.location === curr.location) {
+            if (each.dataValues.request_user_id !== curr.request_user_id
+              && each.dataValues.is_matched === false
+              && each.dataValues.location === curr.location) {
+              isMatched = true;
               userHere = each;
               matchedUser = userHere.dataValues.request_user_id;
+
+              Requests.update({
+                is_matched: true,
+              }, {
+                where: {
+                  request_id: userHere.dataValues.request_id,
+                },
+              }).then().catch();
+
+              Requests.update({
+                is_matched: true,
+              }, {
+                where: {
+                  request_id: curr.request_id,
+                },
+              }).then().catch();
+
               models.Matches.create({
                 user1_id: curr.request_user_id,
                 user2_id: matchedUser,
-              }).then(()=>{
-                console.log("done matching");
+              }).then(() => {
                 res.status(201).json({
                   message: 'match found',
                   userMatched: matchedUser,
                 });
-              }).catch((err)=>{
-                console.log(err);
+              }).catch((err) => {
+                res.status(500).json({ message: err });
               });
-              
-              console.log('done each');
-              throw BreakException;
             }
           });
-          // res.status(201).json({
-          //   message: 'match found',
-          //   user_matched: userHere,
-          // });
-        }).catch((err)=>{
-          console.log(err);
+          if (!isMatched) {
+            res.status(201).json({ message: 'Try again' });
+          }
+        }).catch((err) => {
+          res.status(500).json({ message: err });
         });
       })
       .catch((err) => {
-        if (err == BreakException) {
-          res.status(201).json({
-            message: 'match found',
-            // user_matched: userHere,
-          });
-        } else {
-          res.status(500).json({ message: err });
-        }
+        res.status(500).json({ message: err });
       });
   },
-
-
 };
 
 module.exports = AUTH_CONTROLLER.registerRoute();
