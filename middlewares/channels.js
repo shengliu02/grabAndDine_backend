4ceb98055e85a2d1f2f7da83ff29659485adc4e8
Sheng Liu <sheng1302@gmail.com>
2019-05-04 17:35:36 -0400

    p2p status update

--- middlewares/channels.js
+++ middlewares/channels.js
@@ -1,17 +1,16 @@
 exports = module.exports = function (io) {
-    
   const userOnline = {};
 
   io.on('connection', (socket) => { // userOnline mapping to their nickname!!!
     console.log('user login');
 
     socket.on('new user', (data) => {
-      if (data in userOnline) {
-        console.log('user already exists!');
-      } else {
+      //if (data in userOnline) {
+      //   console.log('user already exists!');
+      // } else {
         const nickname = data;
         userOnline[nickname] = socket; // take their socket ID as their nickname
-      }
+      //}
     });
 
     socket.on('chat message', (from, to, msg) => {
@@ -19,9 +18,20 @@ exports = module.exports = function (io) {
       if (to in userOnline) {
         console.log(`privateMsgTo${to}`);
         userOnline[to].emit(`privateMsgTo${to}`, {
-          from: from,
-          msg: msg
- });
+          from,
+          msg,
+        });
+      }
+    });
+
+    socket.on('updateTypingStatus', (from, to, status) => {
+      console.log('I received a private status update from', from, ' to ', to, status);
+      if (to in userOnline) {
+        console.log(`typingUpdateTo${to}`);
+        userOnline[to].emit(`typingUpdateTo${to}`, {
+          from,
+          status,
+        });
       }
     });
 
